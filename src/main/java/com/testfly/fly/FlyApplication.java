97e67373ac80b4cd7afa7cf8ab93b5a59ea14bfe
zhaoxijian888 <bluebail@163.com>
2019-07-25 20:31:48 +0800

    Zzz

---
+++ src/main/java/com/testfly/fly/FlyApplication.java
@@ -0,0 +1,14 @@
+package com.testfly.fly;
+
+import Test.FunctionTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionTest.class, args);
+    }
+
+}
